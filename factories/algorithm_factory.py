ce13c89b19716599e6fc85c1aa315594792ea92b
Jota98daut <joeldautilio@gmail.com>
2023-06-04 18:41:16 -0300

    Add hill climbing to algorithm factory

--- factories/algorithm_factory.py
+++ factories/algorithm_factory.py
@@ -4,10 +4,16 @@ from constants import *
 
 class AlgorithmFactory:
     @staticmethod
-    def create(algorithm: str, heuristic: Optional[str] = None, **kwargs):
-        if algorithm.lower() in algorithms:
-            return algorithms[algorithm.lower()](heuristic, **kwargs)
+    def create(algorithm: str, heuristic: Optional[Callable[[Node], float]] = None, **kwargs):
+        if algorithm.lower() == HILL_CLIMBING:
+            return algorithms[algorithm](heuristic)
+        elif algorithm.lower() == HILL_CLIMBING_SIDEWAYS:
+            return algorithms[algorithm](heuristic, kwargs.get("max_sideways_moves"))
+        elif algorithm.lower() == HILL_CLIMBING_RANDOM_RESTART:
+            return algorithms[algorithm](heuristic, kwargs.get("exhaustive"),
+                                         kwargs.get("max_iterations"))
         else:
             raise ValueError(f"Algorithm {algorithm} not found")
+
 
 algorithm_factory = AlgorithmFactory()
