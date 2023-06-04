cb1513ce0402dd2085b9494ca897afcb97d4b301
Joel D'Autilio <joeldautilio@gmail.com>
2023-06-04 20:57:29 -0300

    Genetic algorithm constructor

---
+++ algorithms/local/genetic.py
@@ -0,0 +1,31 @@
+import pygad
+
+from algorithms.search_algorithm import Node, SearchAlgorithm
+from problems.problem import Problem
+
+
+class GeneticSearch(SearchAlgorithm):
+    def __init__(self, **kwargs):
+        """Required parameters:
+            - num_generations
+            - num_parents_mating
+        Optional parameters:
+            - fitness_func
+            - sol_per_pop: number of solutions in the population
+            - initial_population
+            - num_genes: number of genes in the chromosome
+            - gene_type
+            - parent_selection_type: 'sss' | 'rws' | 'sus' | 'rank' | 'random' | 'tournament'
+            - keep_parents
+            - keep_elitism
+            - K_tournament
+            - crossover_type
+            - crossover_probability
+            - mutation_type: 'random' | 'swap' | 'scramble' | 'inversion' | 'adaptive'
+            - mutation_probability
+        """
+        super().__init__()
+        self.ga_instance = pygad.GA(**kwargs)
+
+    def search(self, problem: Problem) -> Node:
+        return super().search(problem)
