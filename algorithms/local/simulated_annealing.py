4f3f1d76e3b249f16de5507abca7baccaa436ad4
Jota98daut <joeldautilio@gmail.com>
2023-06-04 18:43:00 -0300

    Make min_temp take effect

--- algorithms/local/simulated_annealing.py
+++ algorithms/local/simulated_annealing.py
@@ -11,9 +11,11 @@ class SimulatedAnnealing(SearchAlgorithm):
         super().__init__()
         self.heuristic = heuristic
         self.schedule = schedule
+        self.min_temp = None
 
     def set_schedule(self, init_temp, decay, min_temp):
-        self.schedule = lambda t: max(min_temp, init_temp * math.pow(math.e, -decay * t))
+        self.schedule = lambda t: init_temp * math.pow(math.e, -decay * t)
+        self.min_temp = min_temp
 
     def search(self, problem: Problem) -> Node:
         if self.schedule is None:
@@ -24,17 +26,13 @@ class SimulatedAnnealing(SearchAlgorithm):
         while True:
             T = self.schedule(t)
 
-            if T <= 0.05:   # with 0.01 it will iterate ~1520 times  (with 0.001 -> ~1980 times)
+            if T <= self.min_temp:   # with 0.01 it will iterate ~1520 times  (with 0.001 -> ~1980 times)
                 return current
 
             succ = random.choice(current.expand(problem))
             delta_e = self.heuristic(current) - self.heuristic(succ)
 
-            if delta_e > 0:
+            if delta_e > 0 or random.random() < math.e ** (delta_e/T):
                 current = succ
-            else:
-                print(delta_e)
-                if random.random() < math.pow(math.e, delta_e/T):  # current <- next , with probability e^{-delta_e/T}
-                    current = succ
 
             t += 1
