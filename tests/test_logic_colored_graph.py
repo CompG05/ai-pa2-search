64af7bd55527d5f75f10772b64b0b5f904171b97
Jota98daut <joeldautilio@gmail.com>
2023-06-08 20:08:33 -0300

    Fix logic_colored_graph
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

--- tests/test_logic_colored_graph.py
+++ tests/test_logic_colored_graph.py
@@ -1,71 +1,76 @@
 from typing import Iterable
+
+from aima.logic import dpll_satisfiable
+from aima.utils import expr
 from structures.graph import Graph
 
 
 def logic_colored_graph(g: Graph, k: int):
-    nodes_list = list(g.nodes())
+    nodes = list(g.nodes())
     e = ""
 
-    def negate_color_for_states(states: Iterable[int], color: int):
-        states_with_color = [f"~V{s},{color}" for s in states]
+    def negate_color_for_states(color: int, states: Iterable):
+        states_with_color = [f"~{var(s, color)}" for s in states]
         return " & ".join(states_with_color)
+    
+    def var(v: str, c: int):
+        return f"{v}{c}"
 
     all_nodes_with_all_colors_list = []
-    for v in range(0, len(nodes_list)):
+    for v in nodes:
         v_with_all_colors_l = []
         for c in range(0, k):
-            v_with_all_colors_l.append(f"V{v},{c}")
+            v_with_all_colors_l.append(f"{var(v, c)}")
         v_with_all_colors = "(" + " | ".join(v_with_all_colors_l) + ")"
 
         all_nodes_with_all_colors_list.append(v_with_all_colors)
     all_nodes_with_all_colors = "(" + " & ".join(all_nodes_with_all_colors_list) + ")"
 
     each_node_only_one_color_l = []
-    for v in range(0, len(nodes_list)):
+    for v in nodes:
         v_only_one_color_l = []
         for c in range(0, k):
-            v_only_c = f"(V{v},{c} => "
+            v_only_c = f"({var(v, c)} ==> "
             other_colors_denied = []
             for other_c in range(0, k):
                 if c == other_c:
                     continue
-                other_colors_denied.append(f"~V{v},{other_c}")
+                other_colors_denied.append(f"~{var(v, other_c)}")
             v_only_c += " & ".join(other_colors_denied) + ")"
             v_only_one_color_l.append(v_only_c)
-        v_only_one_color = "(" + " & ".join(v_only_one_color_l) + ")"
+        v_only_one_color = " & ".join(v_only_one_color_l)
         each_node_only_one_color_l.append(v_only_one_color)
     each_node_only_one_color = "(" + " & ".join(each_node_only_one_color_l) + ")"
 
-
     e = (
-        all_nodes_with_all_colors
-        + " & "
-        + each_node_only_one_color
-        )
+            all_nodes_with_all_colors
+            + " & "
+            + each_node_only_one_color
+    )
 
     adjacents_different_color_l = []
-    for v in range(0, len(nodes_list)):
-        adjacents = [adj for adj in g.get(nodes_list[v]).keys() if adj != v]
+    for v in nodes:
+        adjacents = [adj for adj in g.get(v).keys() if adj != v]
         for c in range(0, k):
-            v_adj_diff_color = negate_color_for_states(adjacents, k)
-            if v_adj_diff_color != "()":
-                adjacents_different_color_l.append(f"(V{v},{c} => {v_adj_diff_color})")
+            v_adj_diff_color = negate_color_for_states(c, adjacents)
+            print(v_adj_diff_color)
+            if v_adj_diff_color != "":
+                adjacents_different_color_l.append(f"({var(v, c)} ==> {v_adj_diff_color})")
     if adjacents_different_color_l:
         adjacents_different_color = "(" + " & ".join(adjacents_different_color_l) + ")"
         e += " & " + adjacents_different_color
 
-
-    print(e)
+    exp = expr(e)
+    result = dpll_satisfiable(exp)
+    print(result)
 
 
 def test_logic_colored_graph():
-    # graph = Graph(dict(
-    # A=dict(B=0, C=0, D=0),
-    # B=dict(E=0),
-    # C=dict(F=0),
-    # D=dict(F=0)
-
-    graph = Graph(dict(A=dict(B=0)))
+    graph = Graph(dict(
+        A=dict(B=0, C=0, D=0),
+        B=dict(E=0),
+        C=dict(F=0),
+        D=dict(F=0)))
 
     logic_colored_graph(graph, 2)
     assert False
