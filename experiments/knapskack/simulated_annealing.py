7e43b7805359928a12ba090d7969b51949087762
PabloRoss7 <pablorossi2001@gmail.com>
2023-06-22 15:06:05 -0300

    Change simulated annealing experiment for knapsack in order to contemplate multiple instances

--- experiments/knapskack/simulated_annealing.py
+++ experiments/knapskack/simulated_annealing.py
@@ -5,48 +5,51 @@ from algorithms.local.simulated_annealing import exp_schedule
 from constants import KNAPSACK, SIMULATED_ANNEALING, ACCUM_VALUE
 from problems.nqueens import NQueensProblem
 from solver.solver import Solver
+import os
 
 
 def main():
-    p = NQueensProblem(dimension=8)
+    configs = [(400, 0.00007, 90000)]
+    iterations = 10
+    file_name_list = ["easy/" + file for file in os.listdir("instances/easy")]
 
-    initial_temperatures = [200]  # [1]
-    cooling_rates = [0.1]  # [0.005, 0.001]
-    limits = [10000]  # [0.000001]
 
-    iterations = 100
-    file_name = "easy/f2_l-d_kp_20_878"
+    for (init_temp, cooling_rate, limit) in configs:
+        print(f"Configuracion = init_temp: {init_temp}, cooling_rate: {cooling_rate}, limit: {limit}")
+        total_solutions = 0
+        for file_name in file_name_list:
+            with open("instances/optimum/" + file_name) as f:
+                optimum = float(f.readline())
+            print("Optimum:", optimum)
 
-    with open("instances/optimum/" + file_name) as f:
-        optimum = int(f.readline())
-    print("Optimum:", optimum)
+            solver = Solver(KNAPSACK, SIMULATED_ANNEALING, ACCUM_VALUE,
+                            problem_kwargs={"path": "instances/" + file_name},
+                            algorithm_kwargs={"schedule": exp_schedule(init_temp, cooling_rate, limit)})
+            solutions = 0
+            times = []
+            values = []
+            last_it_values = []
+            for i in range(iterations):
+                print("\tVoy por:", i, " de ", file_name)
+                bef_time = time.time()
+                solution = solver.solve()
+                solutions += solution.value == optimum
+                aft_time = time.time()
+                times.append(aft_time - bef_time)
+                values.append(solution.value)
+                last_it_values = [s.sack_value for s in solution.path]
+            plt.plot(last_it_values)
+            plt.show()
 
-    for limit in limits:
-        for init_temp in initial_temperatures:
-            for cooling_rate in cooling_rates:
-                solver = Solver(KNAPSACK, SIMULATED_ANNEALING, ACCUM_VALUE,
-                                problem_kwargs={"path": "instances/" + file_name},
-                                algorithm_kwargs={"schedule": exp_schedule(init_temp, cooling_rate, limit)})
-                solutions = 0
-                times = []
-                values = []
-                last_it_values = []
-                for i in range(iterations):
-                    print("Voy por:", i)
-                    bef_time = time.time()
-                    solution = solver.solve()
-                    solutions += solution.value == optimum
-                    aft_time = time.time()
-                    times.append(aft_time - bef_time)
-                    values.append(solution.value)
-                    last_it_values = [s.sack_value for s in solution.path]
-                plt.plot(last_it_values)
-                plt.show()
-                solution_rate = solutions / iterations
-                mean_time = sum(times) / len(times)
-                mean_value = sum(values) / len(values)
-                print(f"\nit: {init_temp}, cr: {cooling_rate}, mt: {limit}, solution rate: {solution_rate}, mean "
-                      f"time: %.4f, mean value: {mean_value}" % mean_time)
+            solution_rate = solutions / iterations
+            mean_time = sum(times) / len(times)
+            mean_value = sum(values) / len(values)
+            print(f"\n\tit: {init_temp}, cr: {cooling_rate}, lt: {limit}, solution rate: {solution_rate}, mean "
+                  f"time: %.4f, mean value: {mean_value}" % mean_time)
+
+            total_solutions += solutions
+
+        print(f"Total solution rate: {total_solutions/(len(file_name_list)*iterations)}")
 
 
 if __name__ == "__main__":
