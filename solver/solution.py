5d99f70e78fb3284c3a5bcc69cb030322816f316
Jota98daut <joeldautilio@gmail.com>
2023-08-03 10:34:28 -0300

    Add problem and algorithm args to Solution
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

--- solver/solution.py
+++ solver/solution.py
@@ -4,12 +4,14 @@ from algorithms.search_algorithm import Node, null_node
 
 
 class Solution:
-    def __init__(self, node: Node, value, algorithm_name, heuristic_name, dtime, memory_peak):
+    def __init__(self, node: Node, value, algorithm_name, heuristic_name, problem_kwargs, algorithm_kwargs, dtime, memory_peak):
         self.final_state = node.state
         self.path = [node.state for node in node.path()]
         self.depth = node.depth
         self.algorithm_name = algorithm_name
         self.heuristic_name = heuristic_name
+        self.problem_kwargs = problem_kwargs
+        self.algorithm_kwargs = algorithm_kwargs
         self.time = dtime
         self.memory = memory_peak
         self.nodes = node.accum_nodes
@@ -17,11 +19,13 @@ class Solution:
 
     @classmethod
     def not_found(cls, algorithm_name: str, heuristic_name: Optional[str]):
-        return Solution(null_node, 0, algorithm_name, heuristic_name, 0, 0)
+        return Solution(null_node, 0, algorithm_name, heuristic_name, {}, {}, 0, 0)
 
     def __str__(self):
         return f"""
 algorithm: {self.algorithm_name}
+heuristic: {self.heuristic_name}
+algorithm arguments: {self.algorithm_kwargs}
 final state: {self.final_state}
 final state value: {self.value}
 time: %.2f ms
@@ -30,8 +34,7 @@ max memory usage: {self.memory} bytes""" % (self.time * 1000)
 
     @classmethod
     def csv_header(cls):
-        return "algorithm,heuristic,solution found,depth,final state,value,time,generated nodes,max memory usage"
+        return "algorithm,heuristic,algorithm args,final state,value,time,generated nodes,max memory usage"
 
     def to_csv(self):
-        return f"{self.algorithm_name},{self.heuristic_name},{self.final_state is not None},{self.depth},\
-        {self.final_state},{self.value},{self.time},{self.nodes},{self.memory}"
+        return f"{self.algorithm_name},{self.heuristic_name},{self.algorithm_kwargs},{self.final_state},{self.value},{self.time},{self.nodes},{self.memory}"
--- solver/solver.py
+++ solver/solver.py
@@ -11,11 +11,11 @@ class Solver:
     def __init__(
         self, problem: str, algorithm: str, heuristic: Optional[str], problem_kwargs=None, algorithm_kwargs=None
     ):
-        problem_kwargs = problem_kwargs or {}
-        algorithm_kwargs = algorithm_kwargs or {}
-        self.problem, heuristic_factory = problem_factory.create(problem, **problem_kwargs)
+        self.problem_kwargs = problem_kwargs or {}
+        self.algorithm_kwargs = algorithm_kwargs or {}
+        self.problem, heuristic_factory = problem_factory.create(problem, **self.problem_kwargs)
         self.heuristic = heuristic_factory.create(heuristic) if heuristic else None
-        self.algorithm = algorithm_factory.create(algorithm, self.heuristic, **algorithm_kwargs)
+        self.algorithm = algorithm_factory.create(algorithm, self.heuristic, **self.algorithm_kwargs)
         self.algorithm_name = algorithm
         self.heuristic_name = heuristic
 
@@ -30,6 +30,8 @@ class Solver:
             value=node_value,
             algorithm_name=self.algorithm_name,
             heuristic_name=self.heuristic_name,
+            problem_kwargs=self.problem_kwargs or {},
+            algorithm_kwargs=self.algorithm_kwargs or {},
             dtime=aft_time - bef_time,
             memory_peak=0,
         )
