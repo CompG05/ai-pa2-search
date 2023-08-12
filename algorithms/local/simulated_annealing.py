ba85102873deaa9596fc730ab41505a32c20da0f
Joel D'Autilio <joeldautilio@gmail.com>
2023-08-12 16:19:28 -0300

    Update simulated annealing
    Co-authored-by: Pablo Rossi <pablorossi2001@gmail.com>

--- algorithms/local/simulated_annealing.py
+++ algorithms/local/simulated_annealing.py
@@ -1,6 +1,7 @@
-from typing import Callable, Optional
+from typing import Callable
 import math
 import random
+import time
 
 import numpy as np
 
@@ -9,30 +10,29 @@ from problems.problem import Problem
 
 
 class SimulatedAnnealing(SearchAlgorithm):
-    def __init__(self, heuristic: Callable[[Node], float], schedule: Optional[Callable[[float], float]] = None):
+    def __init__(self,
+                 heuristic: Callable[[Node], float],
+                 **kwargs):
         super().__init__()
         self.heuristic = heuristic
-        self.schedule = schedule
-
-    def set_schedule(self, k, lam, limit):
-        self.schedule = exp_schedule(k, lam, limit)
+        self.schedule = exp_schedule(kwargs["init_temp"], kwargs["cooling_rate"], kwargs["min_temp"])
+        self.time_limit = kwargs.get("time_limit") or float("inf")
 
     def search(self, problem: Problem) -> Node:
-        if self.schedule is None:
-            raise Exception("Schedule not set")
-
-        current = Node(problem.state_factory.random())
+        s = problem.initial_state or problem.state_factory.random()
+        current: Node = Node(s)
         best = current
         t = 1
+        t0 = time.time()
         while True:
             T = self.schedule(t)
-            if T == 0:
-                return current
+            if T == 0 or time.time() - t0 > self.time_limit or current.state.is_goal():
+                return best
 
             neighbors = current.expand(problem)
 
             if not neighbors:
-                return best.state
+                return best
 
             succ = random.choice(neighbors)
             delta_e = self.heuristic(succ) - self.heuristic(current)
@@ -45,5 +45,10 @@ class SimulatedAnnealing(SearchAlgorithm):
             t += 1
 
 
-def exp_schedule(k, lam, limit):
-    return lambda t: (k * np.exp(-lam * t) if t < limit else 0)
+def exp_schedule(init_temp, cooling_rate, min_temp):
+    def f(t):
+        T = init_temp * np.exp(-cooling_rate * t)
+        return T if T > min_temp else 0
+
+    return f
+
--- factories/algorithm_factory.py
+++ factories/algorithm_factory.py
@@ -4,16 +4,22 @@ from constants import *
 
 class AlgorithmFactory:
     @staticmethod
-    def create(algorithm: str, heuristic: Optional[Callable[[Node], float]] = None, **kwargs):
+    def create(algorithm: str,
+               heuristic: Optional[Callable[[Node], float]] = None,
+               **kwargs):
         if algorithm.lower() == HILL_CLIMBING:
             return algorithms[algorithm](heuristic)
+
         elif algorithm.lower() == HILL_CLIMBING_SIDEWAYS:
             return algorithms[algorithm](heuristic, kwargs.get("max_sideways_moves"))
+
         elif algorithm.lower() == HILL_CLIMBING_RANDOM_RESTART:
             return algorithms[algorithm](heuristic, kwargs.get("exhaustive"),
                                          kwargs.get("time_limit"))
+
+
         elif algorithm.lower() == SIMULATED_ANNEALING:
-            return algorithms[algorithm](heuristic, kwargs.get("schedule"))
+            return algorithms[algorithm](heuristic, **kwargs)
         else:
             raise ValueError(f"Algorithm {algorithm} not found")
 
