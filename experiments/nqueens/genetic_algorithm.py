0c8d0f0cba819aa6552a665a820d6472a4bc8ae7
Jota98daut <joeldautilio@gmail.com>
2023-08-21 15:28:55 -0300

    Refactor nqueens genetic experiment
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

--- experiments/nqueens/genetic_algorithm.py
+++ experiments/nqueens/genetic_algorithm.py
@@ -1,54 +1,62 @@
+import sys
+
 from algorithms.local.genetic import GeneticSearch
-from constants import INVERSE_N_CONFLICTS_FITNESS
+from constants import INVERSE_N_CONFLICTS
 from heuristics.nqueens import NQueensHeuristic
 from problems.nqueens import NQueensProblem
 
 
-def main(dimension, num_generations_list, sol_per_pop_list, selection_method_list, elitism_list):
-    iterations = 100
-    dimension = 8
+def main(dimension, iterations, **params):
     problem = NQueensProblem(dimension=dimension)
-    fitness = NQueensHeuristic().create(INVERSE_N_CONFLICTS_FITNESS)
-    num_genes = dimension
-    for num_generations in num_generations_list:
-        for sol_per_pop in sol_per_pop_list:
-            for selection_method in selection_method_list:
-                for elitism in elitism_list:
-                    solutions = 0
+    fitness = NQueensHeuristic().create_fitness(INVERSE_N_CONFLICTS)
+    solutions = 0
 
-                    for _ in range(iterations):
-                        algorithm = GeneticSearch(
-                            num_generations=num_generations,
-                            num_parents_mating=2,
-                            fitness_func=fitness,
-                            num_genes=num_genes,
-                            sol_per_pop=sol_per_pop,
-                            gene_type=int,
-                            mutation_percent_genes=1 * 100 / num_genes,
-                            gene_space=list(range(0, dimension)),
-                            parent_selection_type=selection_method,
-                            keep_elitism=elitism,
-                        )
-                        best_node = algorithm.search(problem)
-                        # print(fitness[INVERSE_N_CONFLICTS](None, list(best_node.state.data), None))
-                        if best_node.state.is_goal():
-                            solutions += 1
+    for i in range(iterations):
+        print(f"Voy por: {i+1} de {iterations}")
+        algorithm = GeneticSearch(
+            fitness_func=fitness,
+            gene_type=int,
+            num_genes=dimension,
+            mutation_percent_genes=1/8,
+            gene_space=list(range(0, dimension)),
+            **params
+        )
+        best_node = algorithm.search(problem)
+        # print(fitness[INVERSE_N_CONFLICTS](None, list(best_node.state.data), None))
+        if best_node.state.is_goal():
+            solutions += 1
 
-                    print(f"ng: {num_generations}, spp: {sol_per_pop}, sm: {selection_method}, e: {elitism}\n"
-                          f"\tsolution rate: {solutions / iterations}\n")
+    print(f"{params}\n"
+          f"\tsolution rate: {solutions / iterations}\n")
 
 
 if __name__ == "__main__":
-    eight_num_generations_list = [200]
-    eight_sol_per_pop_list = [200]
-    eight_selection_method_list = ["rank", "tournament"]
-    eight_elitism_list = [30, 60]
+    dimension = int(sys.argv[1])
+    iterations = int(sys.argv[2])
 
-    sixteen_num_generations_list = [300]
-    sixteen_sol_per_pop_list = [300]
-    sixteen_selection_method_list = ["rank", "tournament"]
-    sixteen_elitism_list = [30, 60]
+    configs = {
+        8: [
+            {
+                "num_generations": 200,
+                "sol_per_pop": 200,
+                "parent_selection_type": "rank",
+                "num_parents_mating": 2,
+                "keep_elitism": 30,
+                "crossover_type": "single_point"
+            }
+        ],
 
-    #main(8, eight_num_generations_list, eight_sol_per_pop_list, eight_selection_method_list, eight_elitism_list)
+        16: [
+            {
+                "num_generations": 200,
+                "sol_per_pop": 200,
+                "parent_selection_type": "rank",
+                "num_parents_mating": 2,
+                "keep_elitism": 30,
+                # "crossover_type": "single_point"
+            }
+        ]
+    }
 
-    main(16, sixteen_num_generations_list, sixteen_sol_per_pop_list, sixteen_selection_method_list, sixteen_elitism_list)
+    for config in configs[dimension]:
+        main(dimension, iterations, **config)
