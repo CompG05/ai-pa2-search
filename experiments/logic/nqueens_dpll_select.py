4d114f6c968677c05d6540ab90ddb01a7550fcd1
Jota98daut <joeldautilio@gmail.com>
2023-09-05 16:53:28 -0300

    nqueens dpll select exp
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

--- experiments/logic/nqueens_dpll_select.py
+++ experiments/logic/nqueens_dpll_select.py
@@ -1,27 +1,42 @@
-from aima.logic import dpll_select
+import time
+
+from aima.logic import dpll_select, dpll, dpll_satisfiable, dpll_satisfiable_with_count
 from logic.nqueens import to_expr
 
+DIM = 8
 
 def main(et, ps, uc):
-    e = to_expr(8)
+    e = to_expr(DIM)
+    btime = time.time()
     model, count = dpll_select(e, et, ps, uc)
+    atime = time.time()
     print(f"early_termination: {et}, pure_symbol: {ps}, unit_clause: {uc}")
-    if not model:
-        print("Try again capo")
-    else:
-        print(f"Model: {model}")
+    print("Model found:", model is not False)
     print(f"models tested:", {count})
+    print("time: %.2f" % (atime-btime))
+    print()
 
 if __name__ == "__main__":
     params = [
         (True, True, True),
-        # (True, False, False),
-        # (False, True, False),
-        # (False, False, True),
-        # (False, False, False)
         (True, True, False),
         (True, False, True),
         (False, True, True),
+        (True, False, False),
+        (False, True, False),
+        (False, False, True),
+        (False, False, False)
     ]
+
+    e = to_expr(DIM)
+    btime = time.time()
+    model, count = dpll_satisfiable_with_count(e)
+    atime = time.time()
+    print(f"DPLL:")
+    print("Model found:", model is not False)
+    print(f"models tested:", {count})
+    print("time: %.2f" % (atime-btime))
+    print()
+
     for param in params:
         main(*param)
