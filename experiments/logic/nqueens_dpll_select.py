3a63543b923fd9c1b7e62dcf010c3fd196f671d3
Joel D'Autilio <joeldautilio@gmail.com>
2023-09-09 18:46:56 -0300

    dpll_select experiment

--- experiments/logic/nqueens_dpll_select.py
+++ experiments/logic/nqueens_dpll_select.py
@@ -1,21 +1,41 @@
 import time
+import signal
 
-from aima.logic import dpll_select, dpll, dpll_satisfiable, dpll_satisfiable_with_count
+from aima.logic import dpll_select
 from logic.nqueens import to_expr
 
-DIM = 4
+TIME_LIMIT = 5 * 60
+filename = "nqueens_dpll_select_report.txt"
+errors = []
 
-def main(et, ps, uc):
-    e = to_expr(DIM)
-    btime = time.time()
-    model, partial_count, total_count = dpll_select(e, et, ps, uc)
-    atime = time.time()
-    print(f"early_termination: {et}, pure_symbol: {ps}, unit_clause: {uc}")
-    print("Model found:", model is not False)
-    print(f"Partial models reached:", {partial_count})
-    print(f"Total models reached:", {total_count})
-    print("time: %.2f" % (atime-btime))
-    print()
+
+def main(dimension, et, ps, uc):
+    e = to_expr(dimension)
+
+    def handler(_, __):
+        raise TimeoutError()
+
+    signal.signal(signal.SIGALRM, handler)
+
+    if (et, ps, uc) not in errors:
+        signal.alarm(TIME_LIMIT)
+        try:
+            btime = time.time()
+            model, partial_count, total_count = dpll_select(e, et, ps, uc)
+            atime = time.time()
+            with open(filename, "a") as f:
+                f.write(f"{et}\t\t{ps}\t\t{uc}\t\t{partial_count}\t\t{total_count}\t\t%.2f\t\t{model is not False}\n" % (
+                    atime - btime))
+        except TimeoutError:
+            with open(filename, "a") as f:
+                f.write(f"{et}\t\t{ps}\t\t{uc}\t\t \t\t \t\t{5 * 60}\t\tTIMEOUT\n")
+            errors.append((et, ps, uc))
+        except RecursionError:
+            with open(filename, "a") as f:
+                f.write(f"{et}\t\t{ps}\t\t{uc}\t\t \t\t \t\t{5 * 60}\t\tRECURSION EXCEEDED\n")
+            errors.append((et, ps, uc))
+
+
 
 if __name__ == "__main__":
     params = [
@@ -29,15 +49,12 @@ if __name__ == "__main__":
         (False, False, False)
     ]
 
-    e = to_expr(DIM)
-    btime = time.time()
-    model, count = dpll_satisfiable_with_count(e)
-    atime = time.time()
-    print(f"DPLL:")
-    print("Model found:", model is not False)
-    print(f"models tested:", {count})
-    print("time: %.2f" % (atime-btime))
-    print()
-
-    for param in params:
-        main(*param)
+    open(filename, "w").close()
+    for i in [4, 6, 8, 12, 16, 24, 32]:
+        with open(filename, "a") as f:
+            f.write(f"Dimension: {i}\n")
+            f.write("et\t\tps\t\tuc\t\tpc\t\ttc\t\ttime\t\tfound\n")
+        for param in params:
+            main(i, *param)
+        with open(filename, "a") as f:
+            f.write("\n")
