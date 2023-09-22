c77ec8f1e38138089e8813c37b24a66dcf51b750
Joel D'Autilio <joeldautilio@gmail.com>
2023-09-22 00:34:14 -0300

    Add tests for heuristics and inner functions

--- tests/test_knapsack.py
+++ tests/test_knapsack.py
@@ -1,10 +1,13 @@
 import pytest
+from algorithms.search_algorithm import Node
+from constants import ACCUM_VALUE
+from heuristics.knapsack import KnapsackHeuristic
 
 from problems.knapsack import Switch, KnapsackState, KnapsackProblem
 
 #                       0   1   2   3   4   5   6
 weights: list[float] = [5, 10, 30, 15, 40, 25, 50]
-values: list[float] =  [1,  2,  7,  3,  9,  6, 12]
+values: list[float] = [1, 2, 7, 3, 9, 6, 12]
 cap = 75.0
 
 
@@ -44,4 +47,45 @@ def test_action_execute(action, state, expected):
 def test_enabled_actions():
     problem = KnapsackProblem(weights, values, cap, [False] * len(weights))
 
-    assert problem.enabled_actions(s([1, 3])) == [Switch(i) for i in [0, 1, 2, 3, 4, 5, 6]]
+    assert problem.enabled_actions(s([1, 3])) == [
+        Switch(i) for i in [0, 1, 2, 3, 4, 5, 6]
+    ]
+
+
+content_value_pairs = [
+    ([], 0),
+    ([3], 3),
+    ([1, 3, 6], 17),
+]
+
+
+@pytest.mark.parametrize("content, expected", content_value_pairs)
+def test_sack_value(content, expected):
+    state: KnapsackState = s(content)
+
+    assert state.sack_value == expected
+
+
+@pytest.mark.parametrize("content, expected", content_value_pairs)
+def test_accum_value_heuristic(content, expected):
+    h = KnapsackHeuristic().create(ACCUM_VALUE)
+    state: KnapsackState = s(content)
+    node = Node(state)
+
+    assert h(node) == expected
+
+
+@pytest.mark.parametrize(
+    "content, expected",
+    [
+        ([], True),
+        ([3], True),
+        ([1, 3, 6], True),
+        ([1, 3, 6, 5], False),
+        ([4, 6], False),
+    ],
+)
+def test_is_valid(content, expected):
+    state: KnapsackState = s(content)
+
+    assert state.is_valid() == expected
