b15d289d7f46c4d957cb1f558e683aeb49e05d49
Joel D'Autilio <joeldautilio@gmail.com>
2023-09-21 23:21:06 -0300

    Add graph coloring dpll tests

--- tests/test_logic_colored_graph.py
+++ tests/test_logic_colored_graph.py
@@ -1,7 +1,7 @@
 import pytest
 
 from aima.utils import expr, Expr
-from logic.graph_coloring import graph_to_expr
+from logic.graph_coloring import get_graph_coloring_model, graph_to_expr
 from structures.graph import Graph, UndirectedGraph
 
 g1 = UndirectedGraph({"A": {"B": 1}})
@@ -23,3 +23,13 @@ graph_to_expr_config = [
 def test_graph_to_expr(g: Graph, k: int, expected: Expr):
     result = graph_to_expr(g, k)
     assert result == expected
+
+
+@pytest.mark.parametrize("g, k", [(g1, 2), (g2, 2)])
+def test_graph_dpll_returns_model(g, k):
+    assert get_graph_coloring_model(g, k) is not None
+
+
+def test_graph_dpll_returns_correct_model():
+    model = get_graph_coloring_model(g1, 2)
+    assert model == {'A': 0, 'B': 1} or model == {'A': 1, 'B': 0}
